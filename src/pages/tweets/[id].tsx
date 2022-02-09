87a597cfa477d8cfb85d1aa728230f9e5499c11f
Ricardo Costa <ricardo18acs@gmail.com>
2022-02-09 12:58:43 -0300

    feat: internal request

--- src/pages/tweets/[id].tsx
+++ src/pages/tweets/[id].tsx
@@ -1,15 +1,37 @@
+import axios from "axios";
 import { GetServerSideProps, NextPage } from "next";
+import { ReactComponentElement } from "react";
 
 type TweetPageProps = {
   name: string
+  posts: Post[]
+}
+
+type Post = {
+  title: string,
+  body: string
+}
+
+const ShowPosts = (props: { posts: Post[] }) => {
+  return (
+    <>
+    <ul>
+      {
+        props.posts.map((p, key) => <li key={key}>{p.body}</li>)
+      }
+    </ul>
+    </>
+  )
 }
 
 const TweetPage: NextPage<TweetPageProps> = (props: TweetPageProps) => {
   // console.log(props)
+  const { posts } = props
   return (
     <div>
       <h1>Meu Tweet</h1>
       <p>Vales: {props.name}</p>
+      <ShowPosts posts={posts}></ShowPosts>
     </div>
   );
 };
@@ -22,9 +44,13 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
   // acessar db, etc
 
+  // const { data: { posts }} = await axios.get('http://localhost:3000/api/posts/123')
+  const { data: posts } = await axios.get('http://localhost:3000/api/posts/123')
+
   return {
     props: {
-      name: 'Fulk Cycle'
+      name: 'Fulk Cycle',
+      posts
     }
   }
 }
