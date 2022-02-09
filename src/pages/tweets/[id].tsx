68eeec08a4d14ae2d895393ddbb826734ecea949
Ricardo Costa <ricardo18acs@gmail.com>
2022-02-09 11:34:21 -0300

    feat: basic resource

---
+++ src/pages/tweets/[id].tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+
+const TweetPage: NextPage = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>Meu Tweet</h1>
+    </div>
+  );
+};
+
+export default TweetPage
---
+++ src/pages/tweets/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const TweetList: NextPage = () => {
+  return (
+    <div>
+      <h1>Lista de Tweets</h1>
+    </div>
+  );
+};
+
+export default TweetList
